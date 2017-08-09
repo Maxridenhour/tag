6d1902f7442b3d9e3f9c053b90ea256691079fbf
Max Ridenhour <maxridenhour@gmail.com>
2017-08-08 22:49:20 -0500

    Developer v. 1 of app. Added first three activities and icons.

---
+++ app/src/main/java/com/convergesolutions/tag/SplashScreen.java
@@ -0,0 +1,21 @@
+package com.convergesolutions.tag;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);}
+
+    public void Welcome(View view) {
+        Intent intent = new Intent(this, Welcome.class);
+        startActivity(intent);}
+}
--- app/src/main/java/com/convergesolutions/tag/Welcome.java
+++ app/src/main/java/com/convergesolutions/tag/Welcome.java
@@ -1,7 +1,9 @@
 package com.convergesolutions.tag;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Welcome extends AppCompatActivity {
 
@@ -10,4 +12,7 @@ public class Welcome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
     }
+    public void Hashtag(View view) {
+        Intent intent = new Intent(this, Hashtag.class);
+        startActivity(intent);}
 }
